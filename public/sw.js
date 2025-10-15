bbd1c07a329c90763f7622afdf6a2e5381816b11
ChristophLabestin <clabestin@icloud.com>
2025-10-15 21:28:58 +0200

    added pwa functionality for iOS

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE = "app-cache-v1";
+const ASSETS = ["/", "/index.html", "/manifest.webmanifest"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  // Netz zuerst, Fallback Cache
+  e.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy));
+      return res;
+    }).catch(() => caches.match(req))
+  );
+});
